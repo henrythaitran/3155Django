1693b48e1d6463b79c36623e42a24417412c07d5
henrythaitran <henrythaitran03@gmail.com>
2024-09-17 22:49:46 -0400

    4:02:15 - Theme Installation

--- base/views.py
+++ base/views.py
@@ -121,14 +121,13 @@ def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
 
-    if request.user != room.host:
-        return HttpResponse('You are not allowed here!')
-
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
+        form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('home')
+           room = form.save(commit=False)
+           room.host = request.user
+           room.save()
+        return redirect('home')
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
